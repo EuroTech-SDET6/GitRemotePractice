507305bee1fb65275e46a30acf519aba5fdf7186
ustazeynep <zeynep.n.usta@outlook.com>
2023-02-23 19:15:18 +0000

    User class added

---
+++ src/test/java/UserRegistrationTest/User.java
@@ -0,0 +1,15 @@
+package UserRegistrationTest;
+
+public class User {
+
+    public static void main(String[] args) {
+        System.out.println("this is user class");
+
+
+//        code 1
+
+//        code2
+
+
+    }
+}
