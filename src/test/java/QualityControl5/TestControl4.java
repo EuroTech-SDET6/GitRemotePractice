f22c971f447ae3c1f3d49de339211b4aeaf86b98
nihanuygur <nhntsc@gmail.com>
2023-02-24 16:43:39 +0300

    "TestControl5 added"

---
+++ src/test/java/QualityControl5/TestControl4.java
@@ -0,0 +1,14 @@
+package QualityControl5;
+
+public class TestControl4 {
+
+    public static void main(String[] args) {
+
+        System.out.println("TestControl5 is created");
+
+        System.out.println("Time to push");
+
+        // code 123
+
+    }
+}
