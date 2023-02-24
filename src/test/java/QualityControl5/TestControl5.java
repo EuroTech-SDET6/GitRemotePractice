1878a1e654f2da9b2c0c67508623bec80f930dff
Tarik <turgayakcay@gmail.com>
2023-02-24 15:21:46 +0100

    TestControl6 added

---
+++ src/test/java/QualityControl5/TestControl5.java
@@ -0,0 +1,13 @@
+package QualityControl5;
+
+public class TestControl5 {
+    public static void main(String[] args) {
+        System.out.println("Hello guys this is my 6th class");
+
+        // code1
+
+        // code2
+
+        // code3
+    }
+}
