04a216c95dcb9c6d7aedc0aa3958084e13de818e
Tarik <turgayakcay@gmail.com>
2023-02-24 10:38:52 +0100

    test2 is updated

---
+++ src/test/java/QualityControl2/TestControl2.java
@@ -0,0 +1,15 @@
+package QualityControl2;
+
+public class TestControl2 {
+    public static void main(String[] args) {
+        System.out.println("hey guys this is my second message");
+        System.out.println("if you read it, please send me a message");
+
+        // code 1
+
+        // code 2
+
+
+
+    }
+}
